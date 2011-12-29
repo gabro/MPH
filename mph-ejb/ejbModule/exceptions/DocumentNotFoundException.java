b8231d10d0b2339d5be788e75aab732c911634af
Gabriele Petronella <gabriele.petronella@gmail.com>
2011-12-29 18:50:02 +0100

    added some exceptions

---
+++ mph-ejb/ejbModule/exceptions/DocumentNotFoundException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class DocumentNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ mph-ejb/ejbModule/exceptions/GroupEmptyException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class GroupEmptyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
