9001f25a1ea0c4be70e5a80edf8d0b58604c9773
Gabriele Petronella <gabriele.petronella@gmail.com>
2011-12-29 18:21:18 +0100

    added fields to User

--- mph-ejb/ejbModule/mph/User.java
+++ mph-ejb/ejbModule/mph/User.java
@@ -1,8 +1,61 @@
 package mph;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-public abstract class User {
-
+@Table(name="USER")
+public abstract class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	//TODO Avatar field
+	
+	//Getters and setters
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public List<User> getUsers() {
+		//TODO stub
+		return null;
+	}
 }
